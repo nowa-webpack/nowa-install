2b7bbcc8914ec8e8055516bc48680a47963fe0e9
gbk <ck0123456@gmail.com>
2016-05-05 22:53:35 +0800

    ci

---
+++ README.md
@@ -0,0 +1,24 @@
+# nowa-install
+
+[![NPM version](https://img.shields.io/npm/v/nowa-install.svg?style=flat)](https://npmjs.org/package/nowa-install)
+
+nowa plugin installer
+
+---
+
+## Feature
+
+- nowa plugin installer
+
+## Install
+
+```bash
+$ npm i nowa
+$ npm i nowa-install -g
+```
+
+## Usage
+
+```bash
+$ nowa help install
+```
---
+++ src/index.js
@@ -0,0 +1,48 @@
+/*
+* @Author: gbk <ck0123456@gmail.com>
+* @Date:   2016-04-21 17:34:00
+* @Last Modified by:   gbk
+* @Last Modified time: 2016-05-01 19:04:40
+*/
+
+'use strict';
+
+var fs = require('fs');
+var spawn = require('child_process').spawn;
+
+var pkg = require('../package.json');
+
+// default plugin list
+var defaultPlugins = [
+  'init',
+  'build',
+  'lib',
+  'server',
+  'hello'
+];
+
+// plugin defination
+module.exports = {
+
+  command: 'install [plugins...]',
+
+  description: pkg.description,
+
+  action: function(plugins) {
+    if (!plugins.length) {
+      plugins = defaultPlugins;
+    }
+
+    // run npm instal
+    spawn('npm', [
+      'install',
+      '-g',
+      '-d'
+    ].concat(plugins.map(function(plugin) {
+      return 'nowa-' + plugin;
+    })), {
+      stdio: 'inherit',
+      stderr: 'inherit'
+    });
+  }
+};
